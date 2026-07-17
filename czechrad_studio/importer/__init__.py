7c6f0e82d28b2df6e9e9e827946162b38c237e56
hobitvacek <hobithobit@gmail.com>
2026-07-17 20:12:08 +0200

    Export CzechRad importer API

--- czechrad_studio/importer/__init__.py
+++ czechrad_studio/importer/__init__.py
@@ -1,1 +1,23 @@
 """CzechRad source detection, parsing and validation."""
+
+from .czechrad import (
+    CzechRadParseError,
+    ParseFailure,
+    ParsedLog,
+    calculate_checksum,
+    parse_log,
+    parse_measurement_line,
+)
+from .validation import HDOP_NO_FIX, validate_measurement
+
+__all__ = [
+    "CzechRadParseError",
+    "HDOP_NO_FIX",
+    "ParseFailure",
+    "ParsedLog",
+    "calculate_checksum",
+    "parse_log",
+    "parse_measurement_line",
+    "validate_measurement",
+]
+
