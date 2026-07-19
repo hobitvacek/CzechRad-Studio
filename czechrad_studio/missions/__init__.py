11947b4fb58ae3234431d9e24299e6927713cd95
hobitvacek <hobithobit@gmail.com>
2026-07-19 18:39:43 +0200

    Expose stop candidate detection

--- czechrad_studio/missions/__init__.py
+++ czechrad_studio/missions/__init__.py
@@ -1,6 +1,12 @@
 """Mission domain model, use cases and candidate detection."""
 
 from .location_loss import LocationLossEpisode, detect_location_loss_episodes
+from .stops import StopCandidate, detect_stop_candidates
 
-__all__ = ["LocationLossEpisode", "detect_location_loss_episodes"]
+__all__ = [
+    "LocationLossEpisode",
+    "StopCandidate",
+    "detect_location_loss_episodes",
+    "detect_stop_candidates",
+]
 
