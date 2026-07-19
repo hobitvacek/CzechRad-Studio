0cc85502b52ca72498c038a6850bafc989e67976
hobitvacek <hobithobit@gmail.com>
2026-07-19 19:46:44 +0200

    Wire QGIS import workflow

--- czechrad_studio/ui/__init__.py
+++ czechrad_studio/ui/__init__.py
@@ -1,1 +1,7 @@
 """QGIS and Qt6 presentation layer."""
+
+from .import_dialog import ImportDialog
+from .layers import CreatedLayers, add_analysis_layers
+
+__all__ = ["CreatedLayers", "ImportDialog", "add_analysis_layers"]
+
